d338a1de3efb019e68edeca689c9acebf2ecc3e7
JudeJin <jjd1024@foxmail.com>
2024-07-20 14:20:19 -0400

    add db listeners

--- app.js
+++ app.js
@@ -14,22 +14,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/user', userRoutes);
 app.use('/card', cardRoutes);
 
-// jwt auth
-function authenticateToken(req, res, next) {
-    const token = req.headers['authorization'];
-
-    if (!token) return res.sendStatus(401);
-
-    jwt.verify(token, secretKey, async (err, user) => {
-        if (err) return res.sendStatus(403);
-        // Check if the token exists in the database
-        const [users] = await db.query('SELECT * FROM users WHERE id = ? AND authToken = ?', [user.id, token]);
-        if (users.length === 0) return res.sendStatus(403);
-        req.user = user;
-        next();
-    });
-}
-
 // Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
--- src/services/mysql.js
+++ src/services/mysql.js
@@ -5,10 +5,38 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+});
+
+pool.on('connect', (connection) => {
+    console.log(`Connected to database with ID ${connection.threadId} at ${new Date()}`);
+});
+
+pool.on('acquire', (connection) => {
+    console.log(`Connection ${connection.threadId} acquired at ${new Date()}`);
+});
+
+pool.on('release', (connection) => {
+    console.log(`Connection ${connection.threadId} released at ${new Date()}`);
+});
+
+pool.on('enqueue', () => {
+    console.log(`Waiting for available connection slot at ${new Date()}`);
+});
+
+pool.on('error', (err) => {
+    console.error(`Database error: ${err.message}`);
 });
 
 module.exports = {
-    query: (text, params) => pool.query(text, params),
+    query: async (text, params) => {
+        console.log(`Executing SQL query: ${text}`);
+        if (params && params.length) {
+            console.log(`With parameters: ${params}`);
+        }
+        return pool.query(text, params);
+    },
 };
+
 
