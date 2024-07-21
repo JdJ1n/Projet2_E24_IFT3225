fd5a0e4a3b9c7dde939dba942104902e15089a8e
JudeJin <jjd1024@foxmail.com>
2024-07-21 12:27:14 -0400

    add EditableCard template

--- public/private.js
+++ public/private.js
@@ -1,3 +1,5 @@
+var showUsersCards = false;
+
 document.addEventListener('DOMContentLoaded', async (event) => {
 
     try {
@@ -40,15 +42,38 @@ async function loadPage() {
 
     const active_user = await getActiveUser();
 
+    var all_cards = await allCards();
+    var perPage = 10; 
+
     await loadLogoutButton();
 
-    await loadContent(active_user);
+    await loadBar(active_user);
+
+    await updateContents(active_user);
 
     await paintCards();
 
     var addElement = document.getElementById("add");
 
     console.log("Page loaded!")
+}
+
+async function addNavBar(){
+    var usersCardsLink = document.getElementById('usersCards');
+    var allCardsLink = document.getElementById('userPageAllCards');
+    function handleUsersCardsClick() {
+        showUsersCards = true;
+        this.classList.add('active');
+        allCardsLink.classList.remove('active');
+    }
+
+    function handleAllCardsClick() {
+        showUsersCards = false;
+        this.classList.add('active');
+        usersCardsLink.classList.remove('active');
+    }
+    usersCardsLink.addEventListener('click', handleUsersCardsClick);
+    allCardsLink.addEventListener('click', handleAllCardsClick);
 }
 
 async function getActiveUser() {
@@ -68,30 +93,29 @@ async function getActiveUser() {
     }
 }
 
-async function loadContent(activeUser) {
-    document.getElementById("welcomeBar").innerHTML=`
+async function loadBar(activeUser) {
+    document.getElementById("welcomeBar").innerHTML = `
         <h1>Bonjour, ${activeUser.username}</h1>
         <p class="lead">Bienvenue dans le monde des albums de musique !</p>
     `;
     if (activeUser.role === "admin") {
-        loadAdminContent(activeUser);
+        loadAdminBar();
     } else if (activeUser.role === "user") {
-        loadUserContent(activeUser);
+        loadUserBar();
     }
 }
 
-async function loadAdminContent(activeUser) {
+async function loadAdminBar() {
     document.getElementById("top-navbar").innerHTML = `
           <li class="nav-item">
             <a class="nav-link disabled">Vos tuiles</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link active" href="#">Toutes les tuiles</a>
-          </li>
-    `;
+            <a class="nav-link active">Toutes les tuiles</a>
+          </li>`;
 }
 
-async function loadUserContent(activeUser) {
+async function loadUserBar() {
     document.getElementById("top-navbar").innerHTML = `
           <li class="nav-item">
             <a class="nav-link" id="usersCards">Vos tuiles</a>
@@ -99,8 +123,7 @@ async function loadUserContent(activeUser) {
           <li class="nav-item">
             <a class="nav-link active" id="userPageAllCards">Toutes les tuiles</a>
           </li>`;
-    const showUsersCards=false;
-
+    await addNavBar();
 }
 
 async function loadLogoutButton() {
@@ -130,6 +153,18 @@ async function loadLogoutButton() {
     });
 }
 
+async function updateContents(activeUser){
+    if(activeUser.role==='admin'){
+
+    }else if(activeUser.role==='user'){
+        if(showUsersCards){
+
+        }else{
+
+        }
+    }
+}
+
 function createdCard(card) {
     const cardElement = document.createElement('div');
     cardElement.className = 'col-sm-6 col-lg-4 mb-4 rounded';
@@ -150,6 +185,48 @@ function createdCard(card) {
     return cardElement;
 }
 
+function createdEditableCard(card) {
+    const cardElement = document.createElement('div');
+    cardElement.className = 'col-sm-6 col-lg-4 mb-4 rounded';
+    cardElement.innerHTML = `
+                    <div class="card rounded">
+                        <div class="bd-placeholder-img card-img-top">
+                            <img class="rounded-top" src=${card.url} alt="${card.name} - ${card.artist}" height="100%" width="100%" class="bd-placeholder-img">
+                        </div>
+                        <div class="card-body rounded-bottom">
+                            <h4 class="card-title">${card.name} - ${card.artist}</h4>
+                            <h5 class="card-text">${card.category_name}</h5>
+                            <h5 class="card-text">${new Date(card.date).toLocaleDateString()}</h5>
+                            <p class="card-text">${card.description}</p>
+                            <p class="card-text"><small>created by ${card.user_email}</small></p>
+                            <!-- options for edit start -->
+                            <div class="d-flex justify-content-between align-items-center">
+                                <button type="button" class="btn btn-sm btn-secondary" aria-label="Edit">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                    class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                                    <path
+                                    d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z" />
+                                </svg>
+                                <span class="visually-hidden">Edit</span>
+                                </button>
+                                <button type="button" class="btn btn-sm btn-secondary" aria-label="Delete">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash"
+                                    viewBox="0 0 16 16">
+                                    <path
+                                    d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+                                    <path
+                                    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+                                </svg>
+                                <span class="visually-hidden">Delete</span>
+                                </button>
+                            </div>
+                            <!-- options for edit end -->
+                        </div>
+                    </div>
+                `;
+    return cardElement;
+}
+
 async function paintCards() {
     var colors = ['bg-primary text-white', 'bg-success text-white', 'bg-danger text-white', 'bg-warning text-dark', 'bg-info text-dark', 'bg-light text-dark', 'bg-dark text-white'];
 
@@ -161,6 +238,26 @@ async function paintCards() {
 
         card.classList.add(...classes);
     });
+}
+
+async function allCards(){
+    try {
+        const response = await fetch('/card/all_cards', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        } else {
+            const cards = await response.json();
+            return cards
+        }
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 /*
